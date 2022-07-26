797d03685143dd9283e4984f173425fb7a4254a8
SophieZhou <zhoujianyingbest@gmail.com>
2022-07-26 18:41:10 +0800

    'feat:imresize'

--- img.py
+++ img.py
@@ -15,4 +15,20 @@ print (*alst)
 print (fun(a,b, *alst))
 
 # In[]
+'''
+ Preferable interpolation methods are cv2.INTER_AREA for shrinking 缩小
+ and cv2.INTER_CUBIC (slow) & cv2.INTER_LINEAR for zooming放大. By default, 
+ interpolation method used is cv2.INTER_LINEAR for all resizing purposes.
+'''
+import cv2
+import numpy as np
+
+img = cv2.imread('messi.jpg')
+res = cv2.resize(img, None, fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
+#OR
+height, width = img.shape[:2]
+res = cv2.resize(img,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+
+
+# In[]
 
