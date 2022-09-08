ebd9e00a21ba3cafb50ff679d24dd2c0347f6562
SophieZhou <zhoujianyingbest@gmail.com>
2022-09-08 10:50:01 +0800

    'test'

--- img.py
+++ img.py
@@ -20,6 +20,7 @@ print (fun(a,b, *alst))
  and cv2.INTER_CUBIC (slow) & cv2.INTER_LINEAR for zooming放大. By default, 
  interpolation method used is cv2.INTER_LINEAR for all resizing purposes.
 '''
+from xmlrpc.server import _DispatchArity2
 import cv2
 import numpy as np
 
@@ -54,7 +55,10 @@ cv2.imshow('origin',img)
 M = np.float32([[1,0,100],[0,1,50]])
 dst = cv2.warpAffine(img,M,(col,row))
 cv2.imshow('img',dst)
+cv2.imwrite('./res.jpg',dst)
+dst_reorder = dst[:,:,::-1]
 
+cv2.imwrite('./reorder.jpg',dst_reorder[:,:,::-1])
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 # In[]
@@ -82,18 +86,20 @@ plt.subplot(121),plt.imshow(img),plt.title('Input')
 plt.subplot(122),plt.imshow(dst),plt.title('Output')
 plt.show()
 # In[]
+import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
-img = cv2.imread('0028.jpg')
+img = cv2.imread('sudoku1.jpg')
 
 rows,cols,ch = img.shape
 
-pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
-pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+pts1 = np.float32([[156,90],[368,100],[100,330],[400,346]])
+pts2 = np.float32([[0,0],[400,0],[0,400],[400,400]])
 
 M = cv2.getPerspectiveTransform(pts1,pts2)
 
-dst = cv2.warpPerspective(img,M,(600,600))
+dst = cv2.warpPerspective(img,M,(400,400))
 
 plt.subplot(121),plt.imshow(img),plt.title('Input')
 plt.subplot(122),plt.imshow(dst),plt.title('Output')
@@ -107,3 +113,37 @@ print (b)
 # In[]
 
 # In[]
+import cv2
+img = cv2.imread(r"messi.jpg")
+print(img.shape)
+cv2.rectangle(img, (240, 0), (480, 375), (255, 0, 0), 2)
+cv2.imshow("fff", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# In[]
+test = {i:i*2 for i in range(2)}
+print (test)
+
+
+# In[]
+d1 = {1:2,'a':3}
+d2 = {3:'a',4:'b'}
+test = {1:d1,2:d2}
+tmp = test[1]
+tmp['2']='c'
+print (test)
+
+# In[]
+import numpy as np
+
+from numpy.linalg import inv
+a = np.array([[1., 2.], [3., 4.]])
+print(a)
+ainv = inv(a)
+print (ainv)
+
+# In[]
+import numpy as np
+a = np.eye(3)
+print (a)
